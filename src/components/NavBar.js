de8adf4418934c6728ed2f40df84bf1f5674769c
Miguel Angel Veliz <miguelveliz91@gmail.com>
2018-12-05 14:00:37 -0500

    Added a minimal example of a Nav Bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <AppBar color="primary">
+        MICKEY
+      </AppBar>
+    );
+  }
+}
+
+export default NavBar;
